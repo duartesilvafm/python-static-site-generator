d5835b62cf7d677df3d59b0ed27aab19281092b4
Silva <duartesilvafm@gmail.com>
2022-06-25 12:08:22 +0200

    first module commit

---
+++ ssg/site.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import os
+
+class Site():
+
+
+    def __init__(self, source, dest):
+
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+
+    def create_dir(self, path):
+
+        directory = self.dest / path.relative_to(self.source)
+
+        os.mkdir(directory, parents = True, exist_ok = True)
+
+
+    def build(self):
+        
+        os.mkdir(self.dest, parents = True, exist_ok = True)
+
+        for path in self.source.rglob('*'):
+            if os.path.isdir():
+                self.create_dir(path)
