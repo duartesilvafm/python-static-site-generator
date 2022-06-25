0500ab081c7763e9a8666384ac12b3bfcf4d1f3e
Silva <duartesilvafm@gmail.com>
2022-06-25 16:16:49 +0200

    implement parser

---
+++ ssg/parsers.py
@@ -0,0 +1,49 @@
+from typing import List
+from pathlib import Path
+import shutil
+
+
+class Parser():
+
+    
+    def __init__(self, extensions: List(str) = []):
+        self.extensions = extensions
+
+    
+    def valid_extensions(self, extension):
+        return extension in self.extensions
+
+    
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+
+    def read(self, path):
+        with open(path) as file:
+            return file.read()
+
+
+    def write(self, path, dest, content, ext = '.html'):
+
+        full_path = self.dest / path.with_suffix(ext).name
+
+        with open(full_path) as file:
+            file.write(content)
+
+    
+    def copy(self, path, source, dest):
+
+        tocopy = dest / path.relative_to(source)
+
+        shutil.copy2(src = path, dst = tocopy)
+
+
+class ResourceParser(Parser):
+
+
+    extensions = ['.jpg', '.png', '.gif', '.css', '.html']
+
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        super().copy(path, source, dest)
+
--- ssg/site.py
+++ ssg/site.py
@@ -4,10 +4,11 @@ import os
 class Site():
 
 
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers = None):
 
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
 
     def create_dir(self, path):
@@ -22,5 +23,27 @@ class Site():
         self.dest.mkdir(parents = True, exist_ok = True)
 
         for path in self.source.rglob("*"):
+
             if path.is_dir():
                 self.create_dir(path)
+
+            elif path.is_file():
+                self.run_parser(path)
+
+    
+    def load_parsers(self, extension):
+
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    
+    def run_parser(self, path):
+
+        parser = self.load_parser(path.suffix)
+
+        if parser != None:
+            parser.parse(path=path, source=self.source, dest=self.dest)
+
+        else:
+            print('Not Implemented')
